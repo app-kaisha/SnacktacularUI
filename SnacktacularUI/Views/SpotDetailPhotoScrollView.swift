57041219a1740378f3453ed465752aa27bf8693f
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-24 21:03:18 +0000

    Horizontal scroll view factored out to own view

---
+++ SnacktacularUI/Views/SpotDetailPhotoScrollView.swift
@@ -0,0 +1,19 @@
+//
+//  SpotDetailPhotoScrollView.swift
+//  SnacktacularUI
+//
+//  Created by app-kaihatsusha on 24/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpotDetailPhotoScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotDetailPhotoScrollView()
+}
